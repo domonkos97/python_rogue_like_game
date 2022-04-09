5728c38854db53557805fdb54dfa25f5c7dcb324
Domonkos Szabo <szabo.domonkos97@gmail.com>
2022-04-09 17:57:58 +0200

    Creating files: controll, ui, util. Adding descriptions to files.

---
+++ main.py
@@ -0,0 +1,22 @@
+'''
+This file defines the game logic (main logic), with it only containing one function. 
+At the end of the file, there are ideas for further improvment of the game.
+'''
+
+
+def main():
+    '''
+    The main function defines the game logic, using functions from UI and the controller.
+    '''
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
+# SUGGESTIONS TO IMPROVE THE GAME:
+    # create a database
+    # saves usernames, their highest scores (hishest level)
+    # need module.py !!
